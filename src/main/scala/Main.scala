1fc5a291247ddafd7e442efb42c8138e82361973
Johannes Wirbser <johannes.wirbser@gmx.de>
2021-10-26 09:03:44 +0200

    Some little initial code with test. First, only alternating between player.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,20 @@
-@main def hello: Unit = 
-  println("Hello world!")
+import java.util.Scanner
+
+@main def hello: Unit =
+  println("I am cardgame Phase10!")
   println(msg)
 
+  val s = new Scanner(System.in)
+  val players = s.nextLine().split(" ")
+  var current_player = 0
+
+  while(true)
+    println("Aktueller Spieler: " + players(current_player))
+    println("Auszutauschende Karte angeben")
+    Thread.sleep(1000)
+
+    current_player = nextPlayer(current_player, players.length)
+
 def msg = "I was compiled by Scala 3. :)"
+
+private def nextPlayer(currentPlayer:Int, numberOfPlayers:Int):Int = (currentPlayer + 1) % numberOfPlayers
--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -1,6 +1,9 @@
-import org.junit.Test
-import org.junit.Assert.*
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
 
 class Test1:
-  @Test def t1(): Unit = 
-    assertEquals("I was compiled by Scala 3. :)", msg)
+    msg should be("I was compiled by Scala 3. :)")
+    nextPlayer(0, 4) should be(1)
+    nextPlayer(1, 4) should be(2)
+    nextPlayer(2, 4) should be(3)
+    nextPlayer(3, 4) should be(0)
