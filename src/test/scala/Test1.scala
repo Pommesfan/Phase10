367e07aaeb82f44f83b7eb6559c52dab2e4a3bed
= <=>
2021-10-28 11:33:16 +0200

    test for inner class Card implemented

--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -1,17 +1,29 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
-class Test1:
+class Test1 extends AnyWordSpec:
+    val card = Card(2, 5)
+    card.color should be(2)
+    card.value should be(5)
+    card.toString should be("Farbe: Gelb; Wert = 5")
+
+    val cards_to_compare = List(Card(3,8), Card(3,8), Card(4,8), Card(3,9), Card(1,7))
+
+    cards_to_compare(0).equals(cards_to_compare(1)) should be(true)
+    cards_to_compare(0).equals(cards_to_compare(2)) should be(false)
+    cards_to_compare(0).equals(cards_to_compare(3)) should be(false)
+    cards_to_compare(0).equals(cards_to_compare(4)) should be(false)
+
     msg should be("I was compiled by Scala 3. :)")
     nextPlayer(0, 4) should be(1)
     nextPlayer(1, 4) should be(2)
     nextPlayer(2, 4) should be(3)
     nextPlayer(3, 4) should be(0)
-    
+
     val c = createCard
     c.color >= 1 && c.color <= 4 should be(true)
     c.value >= 1 && c.value <= 12 should be(true)
-    
+
     val stash = createCardStash(2)
     stash.size should be(2)
     for (l <- stash)
