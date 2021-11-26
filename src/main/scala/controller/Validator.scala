fc9f444a90110c9e781d80ffedb87b82e13ec92e
= <=>
2021-11-26 20:39:15 +0100

    All phases implemented.

--- src/main/scala/controller/Validator.scala
+++ src/main/scala/controller/Validator.scala
@@ -10,6 +10,14 @@ object Validator {
   def getValidator(index : Int): ValidatorStrategy = index match {
     case 1 => new Phase1Validator
     case 2 => new Phase2Validator
+    case 3 => new Phase3Validator
+    case 4 => new Phase4Validator
+    case 5 => new Phase5Validator
+    case 6 => new Phase6Validator
+    case 7 => new Phase7Validator
+    case 8 => new Phase8Validator
+    case 9 => new Phase9Validator
+    case 10 => new Phase10Validator
   }
 }
 
@@ -44,7 +52,38 @@ private class Phase1Validator extends ValidatorStrategy(1):
   override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.MULTIPLES, 3), new CardGroup(GroupType.MULTIPLES, 3))
   override def description: String = "Zwei Drillinge"
 
-
 private class Phase2Validator extends ValidatorStrategy(2):
   override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.MULTIPLES, 3), new CardGroup(GroupType.SEQUENCE, 4))
   override def description: String = "Drilling und Viererfolge"
+
+private class Phase3Validator extends ValidatorStrategy(3):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.MULTIPLES, 4), new CardGroup(GroupType.SEQUENCE, 4))
+  override def description: String = "Vierling und Viererfolge"
+
+private class Phase4Validator extends ValidatorStrategy(4):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.SEQUENCE, 7))
+  override def description: String = "Siebenerfolge"
+
+private class Phase5Validator extends ValidatorStrategy(5):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.SEQUENCE, 8))
+  override def description: String = "Achterfolge"
+
+private class Phase6Validator extends ValidatorStrategy(6):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.SEQUENCE, 9))
+  override def description: String = "Neunerfolge"
+
+private class Phase7Validator extends ValidatorStrategy(7):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.MULTIPLES, 4), new CardGroup(GroupType.MULTIPLES, 4))
+  override def description: String = "Zwei Vierlinge"
+
+private class Phase8Validator extends ValidatorStrategy(8):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.SAME_COLOR, 7))
+  override def description: String = "Sieben Karten einer Farbe"
+
+private class Phase9Validator extends ValidatorStrategy(9):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.MULTIPLES, 5), new CardGroup(GroupType.MULTIPLES, 2))
+  override def description: String = "Fünfling und Zwilling"
+
+private class Phase10Validator extends ValidatorStrategy(10):
+  override protected val cardGroups: List[CardGroup] = List(new CardGroup(GroupType.MULTIPLES, 5), new CardGroup(GroupType.MULTIPLES, 3))
+  override def description: String = "Fünfling und Drilling"
--- src/test/scala/controller/ValidatorSpec.scala
+++ src/test/scala/controller/ValidatorSpec.scala
@@ -46,5 +46,93 @@ class ValidatorSpec extends AnyWordSpec {
         v.validate(cards2, indices(List(3,4))) should be(false)
       }
     }
+    "validates phase 3" when {
+      val v = Validator.getValidator(3)
+      val cards1 = List(Card(1, 3), Card(3, 3), Card(4, 3), Card(2, 3), Card(1, 8), Card(3, 9), Card(1, 10), Card(3,11))
+      val cards2 = List(Card(2, 5), Card(1, 9), Card(3, 5), Card(4, 5), Card(3, 8), Card(1, 9), Card(2, 10), Card(1, 12))
+      "validate phase 3 with suitable cards" in {
+        v.validate(cards1, indices(List(4,4))) should be(true)
+      }
+      "validate phase 3 with unsuitable cards" in {
+        v.validate(cards2, indices(List(4,4))) should be(false)
+      }
+    }
+    "validates phase 4" when {
+      val v = Validator.getValidator(4)
+      val cards1 = List(Card(1, 3), Card(3, 4), Card(4, 5), Card(2, 6), Card(1, 7), Card(3, 8), Card(1, 9))
+      val cards2 = List(Card(2, 5), Card(1, 6), Card(3, 7), Card(4, 8), Card(3, 9), Card(1, 10), Card(2, 12))
+      "validate phase 4 with suitable cards" in {
+        v.validate(cards1, indices(List(7))) should be(true)
+      }
+      "validate phase 4 with unsuitable cards" in {
+        v.validate(cards2, indices(List(7))) should be(false)
+      }
+    }
+    "validates phase 5" when {
+      val v = Validator.getValidator(5)
+      val cards1 = List(Card(1, 2), Card(3, 3), Card(4, 4), Card(2, 5), Card(1, 6), Card(3, 7), Card(1, 8), Card(2,9))
+      val cards2 = List(Card(2, 11), Card(1, 12), Card(3, 1), Card(4, 2), Card(3, 4), Card(1, 5), Card(2, 6), Card(3,7))
+      "validate phase 5 with suitable cards" in {
+        v.validate(cards1, indices(List(8))) should be(true)
+      }
+      "validate phase 5 with unsuitable cards" in {
+        v.validate(cards2, indices(List(8))) should be(false)
+      }
+    }
+    "validates phase 6" when {
+      val v = Validator.getValidator(6)
+      val cards1 = List(Card(1, 9), Card(3, 10), Card(4, 11), Card(2, 12), Card(1, 1), Card(3, 2), Card(1, 3), Card(1, 4), Card(1, 5))
+      val cards2 = List(Card(2, 8), Card(1, 9), Card(3, 10), Card(4, 11), Card(3, 12), Card(1, 1), Card(2, 2), Card(1, 3), Card(1, 5))
+      "validate phase 6 with suitable cards" in {
+        v.validate(cards1, indices(List(9))) should be(true)
+      }
+      "validate phase 6 with unsuitable cards" in {
+        v.validate(cards2, indices(List(9))) should be(false)
+      }
+    }
+    "validates phase 7" when {
+      val v = Validator.getValidator(7)
+      val cards1 = List(Card(1, 4),Card(3,4),Card(2,4),Card(2,4), Card(2,9), Card(3,9),Card(2,9),Card(4,9))
+      val cards2 = List(Card(2, 5), Card(1, 5), Card(3, 5), Card(4, 5), Card(3, 3), Card(1, 9), Card(2, 3), Card(4,3))
+      "validate phase 7 with suitable cards" in {
+        v.validate(cards1, indices(List(4,4))) should be(true)
+      }
+      "validate phase 7 with unsuitable cards" in {
+        v.validate(cards2, indices(List(4,4))) should be(false)
+      }
+    }
+    "validates phase 8" when {
+      val v = Validator.getValidator(8)
+      val cards1 = List(Card(1, 3), Card(1, 3), Card(1, 3), Card(1, 7), Card(1, 8), Card(1, 9), Card(1, 10))
+      val cards2 = List(Card(4, 5), Card(4, 9), Card(4, 5), Card(4, 7), Card(2, 8), Card(4, 11), Card(4, 12))
+      "validate phase 8 with suitable cards" in {
+        v.validate(cards1, indices(List(7))) should be(true)
+      }
+      "validate phase 8 with unsuitable cards" in {
+        v.validate(cards2, indices(List(7))) should be(false)
+      }
+    }
+    "validates phase 9" when {
+      val v = Validator.getValidator(9)
+      val cards1 = List(Card(1, 6), Card(4, 6), Card(1, 6), Card(2, 6), Card(1, 6), Card(4, 8), Card(4, 8))
+      val cards2 = List(Card(4, 5), Card(4, 5), Card(4, 5), Card(4, 5), Card(2, 5), Card(4, 11), Card(4, 12))
+      "validate phase 9 with suitable cards" in {
+        v.validate(cards1, indices(List(5,2))) should be(true)
+      }
+      "validate phase 9 with unsuitable cards" in {
+        v.validate(cards2, indices(List(5,2))) should be(false)
+      }
+    }
+    "validates phase 10" when {
+      val v = Validator.getValidator(10)
+      val cards1 = List(Card(1, 3), Card(3, 3), Card(4, 3), Card(2, 3), Card(1, 3), Card(3, 10), Card(1, 10), Card(4, 10))
+      val cards2 = List(Card(2, 2), Card(1, 2), Card(3, 2), Card(4, 2), Card(3, 2), Card(1, 5), Card(2, 11), Card(1, 5))
+      "validate phase 10 with suitable cards" in {
+        v.validate(cards1, indices(List(5,3))) should be(true)
+      }
+      "validate phase 10 with unsuitable cards" in {
+        v.validate(cards2, indices(List(5,3))) should be(false)
+      }
+    }
   }
 }
