432a4415d51f000f38f10d0bddf9a9f302b8b3c6
= <=>
2022-01-14 09:26:36 +0100

    Image in StartScreen included and bug in undo of init players fixed.

--- src/main/scala/aview/Scenes/StartScreen.scala
+++ src/main/scala/aview/Scenes/StartScreen.scala
@@ -19,6 +19,7 @@ class StartScreen(controller: ControllerInterface) extends Scene {
     padding = Insets(20,20,20,20)
     spacing = 20
     children = Seq(
+      new ImageView("Phase10-Logo.png"),
       new Label("Spielernamen eingeben"),
       input,
       new Button("Spielen") {
